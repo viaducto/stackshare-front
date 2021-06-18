30a24c91579b9a3433f64742bca0bd41395076ea
Jorge Rojas <jl.rojas@viaducto.io>
2021-06-18 11:07:52 -0500

    add: tab preferences in group drawer

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -50,27 +50,27 @@ const Styled = styled.div`
   }
 `;
 
-const WrapTable = styled.div`
-  table {
-    min-width: 60vw;
-    thead,
-    tbody {
-      min-width: 60vw;
-    }
-    tbody > tr {
-      max-width: 60vw !important;
-    }
-  }
-`;
-
 const Shop = () => {
   const [openTable, setOpenTable] = useState(false);
   const [openshop, setOpenshop] = useState(false);
   const [openConfig, setOpenConfig] = useState(false);
   const [tab, setTab] = useState(0);
+  const [tabDrawer, setTabDrawer] = useState(0);
   const [configuration, setConfiguration] = useState(0);
   const [activeTooltip, setActiveTooltip] = useState(false);
   const [appName, setAppName] = useState('App name');
+  const WrapTable = styled.div`
+    table {
+      min-width: ${tab === 2 ? '63vw' : '60vw'};
+      thead,
+      tbody {
+        min-width: ${tab === 2 ? '63vw' : '60vw'};
+      }
+      tbody > tr {
+        max-width: ${tab === 2 ? '63vw' : '60vw'} !important;
+      }
+    }
+  `;
 
   return (
     <BodyContent
@@ -457,7 +457,7 @@ const Shop = () => {
                 <Container
                   horizontal='md'
                   style={{
-                    width: '23rem',
+                    width: '20rem',
                     border: '0.063rem solid #BFBFBF',
                     borderRadius: 4,
                     textAlign: 'center',
@@ -545,6 +545,7 @@ const Shop = () => {
           </div>
         )}
       </BodyMain>
+
       <Drawer active={openTable} onClose={() => setOpenTable(false)}>
         <div style={{ backgroundColor: 'white', height: '100%' }}>
           <div style={{ borderBottom: '0.063rem solid #d9d9d9' }}>
@@ -570,55 +571,88 @@ const Shop = () => {
               />
             </Container>
           </div>
-          <div style={{ borderBottom: '0.063rem solid #d9d9d9' }}>
-            <Container vertical='md' left='md' right='md'>
-              <div
-                style={{
-                  display: 'flex',
-                  justifyContent: 'flex-start',
-                  alignItems: 'center',
-                  gap: '0.5rem',
-                }}
-              >
-                <Paragraph>App configuration</Paragraph>
-                <Tooltip
-                  active={activeTooltip}
-                  position='bottom'
-                  color='#262626'
-                  textColor='#fff'
-                  label={
-                    'Choose the type of configurationAdvanced configuration: finetune usages of data and bandwithSmart configuration: Define a budget for unlimited data and calculated bandwith'
-                  }
-                >
-                  <div
-                    onMouseOver={() => setActiveTooltip(true)}
-                    onMouseLeave={() => setActiveTooltip(false)}
-                    style={{ width: 14, height: 14, backgroundColor: 'gray', borderRadius: '50%' }}
-                  />
-                </Tooltip>
-              </div>
-              <Spacer size='xs' direction='vertical' />
-              <Container right='lg' style={{ width: '90%' }}>
-                <Select
-                  size='sm'
-                  radius={0.125}
-                  border={{
-                    top: '0.063rem solid #d9d9d9',
-                    right: '0.063rem solid #d9d9d9',
-                    bottom: '0.063rem solid #d9d9d9',
-                    left: '0.063rem solid #d9d9d9',
-                  }}
-                  onChange={(e: any) => {
-                    setConfiguration(parseInt(e.target.value));
+          <Container horizontal='md' vertical='sm'>
+            <Tab
+              horizontalSpacing='sm'
+              icon={null}
+              iconSpacing='none'
+              onClick={() => setTabDrawer(0)}
+              text='Preferences'
+              transition='ease'
+              type='tab'
+              verticalSpacing='sm'
+              id='personal'
+            />
+            <Tab
+              horizontalSpacing='sm'
+              icon={null}
+              iconSpacing='none'
+              onClick={() => setTabDrawer(1)}
+              text='Group Access'
+              transition='ease'
+              type='tab'
+              verticalSpacing='sm'
+              id='group'
+            />
+          </Container>
+
+          {tabDrawer === 0 && (
+            <div style={{ borderBottom: '0.063rem solid #d9d9d9' }}>
+              <Container vertical='md' left='md' right='md'>
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                    gap: '0.5rem',
                   }}
                 >
-                  <option value='0'>Advanced configuration</option>
-                  <option value='1'>Smart configuration</option>
-                </Select>
+                  <Paragraph>App configuration</Paragraph>
+                  <Tooltip
+                    active={activeTooltip}
+                    position='bottom'
+                    color='#262626'
+                    textColor='#fff'
+                    label={
+                      'Choose the type of configurationAdvanced configuration: finetune usages of data and bandwithSmart configuration: Define a budget for unlimited data and calculated bandwith'
+                    }
+                  >
+                    <div
+                      onMouseOver={() => setActiveTooltip(true)}
+                      onMouseLeave={() => setActiveTooltip(false)}
+                      style={{
+                        width: 14,
+                        height: 14,
+                        backgroundColor: 'gray',
+                        borderRadius: '50%',
+                      }}
+                    />
+                  </Tooltip>
+                </div>
+                <Spacer size='xs' direction='vertical' />
+                <Container right='lg' style={{ width: '90%' }}>
+                  <Select
+                    size='sm'
+                    radius={0.125}
+                    border={{
+                      top: '0.063rem solid #d9d9d9',
+                      right: '0.063rem solid #d9d9d9',
+                      bottom: '0.063rem solid #d9d9d9',
+                      left: '0.063rem solid #d9d9d9',
+                    }}
+                    onChange={(e: any) => {
+                      setConfiguration(parseInt(e.target.value));
+                    }}
+                  >
+                    <option value='0'>Advanced configuration</option>
+                    <option value='1'>Smart configuration</option>
+                  </Select>
+                </Container>
               </Container>
-            </Container>
-          </div>
-          {configuration === 0 && (
+            </div>
+          )}
+
+          {configuration === 0 && tabDrawer === 0 && (
             <>
               <Container vertical='lg' horizontal='md' style={{ width: '95%' }}>
                 <Paragraph lineHeight='1.375rem' weight='600' color='dark'>
@@ -812,7 +846,8 @@ const Shop = () => {
               </Container>
             </>
           )}
-          {configuration === 1 && (
+
+          {configuration === 1 && tabDrawer === 0 && (
             <>
               <Container vertical='md' left='md' right='md'>
                 <Paragraph lineHeight='1.375rem' weight='600' color='dark'>
@@ -852,8 +887,126 @@ const Shop = () => {
               </Container>
             </>
           )}
+
+          {tabDrawer === 1 && (
+            <>
+              <Container right='lg' left='md' bottom='sm' style={{ height: '80%' }}>
+                <div
+                  style={{
+                    border: '0.063rem solid #d9d9d9',
+                    width: '25.75rem',
+                    minHeight: '14.938rem',
+                  }}
+                >
+                  <Container
+                    vertical='sm'
+                    horizontal='sm'
+                    style={{ borderBottom: '0.063rem solid #d9d9d9' }}
+                  >
+                    <Paragraph lineHeight='1.375rem' color='#595959'>
+                      Access Groups
+                    </Paragraph>
+                  </Container>
+                  <Container top='xs' bottom='sm' horizontal='xs'>
+                    <Input label='Search here' icon='search' borderColor='#d9d9d9' />
+                  </Container>
+                  <Container
+                    bottom='md'
+                    left='xs'
+                    right='sm'
+                    style={{ borderBottom: '0.063rem solid #d9d9d9' }}
+                  >
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                      <Paragraph lineHeight='1.375rem' color='#000000'>
+                        Supergroup
+                      </Paragraph>
+                      <Paragraph lineHeight='1.375rem' color='#9A9494'>
+                        14 members
+                      </Paragraph>
+                    </div>
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                      <Paragraph lineHeight='1.375rem' color='#000000'>
+                        8th Graders
+                      </Paragraph>
+                      <Paragraph lineHeight='1.375rem' color='#9A9494'>
+                        1,984 members
+                      </Paragraph>
+                    </div>
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                      <Paragraph lineHeight='1.375rem' color='#000000'>
+                        Teachers
+                      </Paragraph>
+                      <Paragraph lineHeight='1.375rem' color='#9A9494'>
+                        201 members
+                      </Paragraph>
+                    </div>
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                      <Paragraph lineHeight='1.375rem' color='#000000'>
+                        Blink 182
+                      </Paragraph>
+                      <Paragraph lineHeight='1.375rem' color='#9A9494'>
+                        3 members
+                      </Paragraph>
+                    </div>
+                  </Container>
+                  <Container
+                    vertical='xs'
+                    horizontal='xs'
+                    style={{ display: 'flex', justifyContent: 'flex-end' }}
+                  >
+                    <Button
+                      label='Button'
+                      variant='outline'
+                      size='small'
+                      colors={{
+                        text: '#595959',
+                        default: '#595959',
+                      }}
+                    />
+                  </Container>
+                </div>
+                <Container top='md'>
+                  <GroupAccessUser apps='18' groups='Super Group' />
+                  <GroupAccessUser apps='28' groups='Teachers, 1 more..' />
+                  <GroupAccessUser apps='3' groups='Teachers, 2 more..' />
+                  <GroupAccessUser apps='8' groups='Teachers, 1 more..' />
+                  <GroupAccessUser apps='82' groups='Teachers, 1 more..' />
+                </Container>
+              </Container>
+              <Container
+                right='lg'
+                left='md'
+                style={{ height: '4rem', boxShadow: ' 0px -4px 12px rgba(0, 0, 0, 0.05)' }}
+              >
+                <div
+                  style={{
+                    height: '100%',
+                    position: 'sticky',
+                    bottom: 0,
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    width: '100%',
+                  }}
+                >
+                  <Paragraph color='#000000'>Monthly price calculated at market value</Paragraph>
+                  <Button
+                    label='Add app'
+                    colors={{
+                      default: '#096DD9',
+                      hover: '#0254ac',
+                      click: '#096dd9bc',
+                      text: '#fff',
+                    }}
+                    onClick={() => setOpenTable(false)}
+                  />
+                </div>
+              </Container>
+            </>
+          )}
         </div>
       </Drawer>
+
       <Drawer active={openshop} onClose={() => setOpenshop(false)}>
         <div style={{ backgroundColor: 'white', height: '100%' }}>
           <div style={{ borderBottom: '0.063rem solid #d9d9d9' }}>
@@ -972,6 +1125,7 @@ const Shop = () => {
           </Container>
         </div>
       </Drawer>
+
       <Drawer
         active={openConfig}
         onClose={() => {
@@ -1049,6 +1203,7 @@ const AppsAccumulated = ({ app }: any) => {
             padding: '0 0 0 0.5rem',
             display: 'flex',
             gap: '0.563rem',
+            alignItems: 'center',
           }}
         >
           <img src='https://i.pravatar.cc/32' alt='avatar' style={{ borderRadius: '50%' }} />
@@ -1065,3 +1220,34 @@ const AppsAccumulated = ({ app }: any) => {
     </>
   );
 };
+
+const GroupAccessUser = ({ apps, groups }: { apps: string; groups: string }) => (
+  <Container bottom='md'>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <Container right='md' style={{ display: 'flex', alignItems: 'center' }}>
+        <img src='https://i.pravatar.cc/38' alt='avatar' style={{ borderRadius: '50%' }} />
+      </Container>
+      <div style={{ width: '100%' }}>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+            <Paragraph lineHeight='1.375rem' color='#3E3F42' weight='600'>
+              Daniel Padilla
+            </Paragraph>
+            <Spacer direction='horizontal' size='sm' />
+            <Paragraph lineHeight='1.375rem' color='#40A9FF' weight='500'>
+              {groups}
+            </Paragraph>
+          </div>
+          <div>
+            <Paragraph lineHeight='1.375rem' color='#9A9494' align='right'>
+              {apps} apps
+            </Paragraph>
+          </div>
+        </div>
+        <div>
+          <Anchor label='daniel.padilla@hotmail.com' size='sm' color='#9EA0A5' />
+        </div>
+      </div>
+    </div>
+  </Container>
+);
