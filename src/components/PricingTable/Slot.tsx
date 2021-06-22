007f84d157fdf70b3572b01efabddc05a8f1e34d
jp-olvera <jp.olvera@viaducto.io>
2021-06-22 10:55:48 -0500

    feat: refactor styles

--- src/components/PricingTable/Slot.tsx
+++ src/components/PricingTable/Slot.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
 import styled from 'styled-components';
 const StyledSlot = styled.button`
-  margin: 0;
-  padding: 0;
+  appearance: none;
+  background-color: rgba(124, 179, 5, 0.4);
   box-sizing: border-box;
   border: none;
+  border-radius: 0rem;
+  box-sizing: border-box;
   cursor: pointer;
-  background-color: rgba(124, 179, 5, 0.4);
+  display: flex;
+  margin: 0;
+  padding: 0;
+  padding: 0 0.68rem;
   position: absolute;
   width: 100%;
-  box-sizing: border-box;
-  padding-left: 5px;
-  border-radius: 0rem;
   z-index: 1;
+
   &::before {
+    background-color: #7cb305;
     content: '';
-    position: absolute;
-    left: 0;
     height: 100%;
+    left: 0;
+    position: absolute;
     top: 0;
     width: 0.229rem;
-    background-color: #7cb305;
   }
+
   &:hover {
     box-shadow: rgba(124, 179, 5, 0.6) 0px 0px 0px 3px;
   }
@@ -35,8 +38,8 @@ interface SlotProps {
 const Slot = ({ title, timeLapse, top, height, ...rest }: SlotProps) => {
   return (
     <StyledSlot type='button' style={{ top, height }} {...rest}>
-      <span style={{ fontSize: '1rem' }}>{title} </span>
-      <span style={{ fontSize: '0.8rem' }}>{timeLapse}</span>
+      <span style={{ fontSize: '1rem', textTransform: 'capitalize' }}>{title} </span>
+      <span style={{ fontSize: '0.8rem', marginLeft: 'auto' }}>{timeLapse}</span>
     </StyledSlot>
   );
 };
