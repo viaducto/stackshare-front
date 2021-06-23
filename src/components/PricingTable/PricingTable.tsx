61c250de98597d99414bed3a5a1e1ec54a46a456
jp-olvera <jp.olvera@viaducto.io>
2021-06-23 12:25:17 -0500

    feat: fix week dates and update params structure

--- src/components/PricingTable/PricingTable.tsx
+++ src/components/PricingTable/PricingTable.tsx
@@ -2,22 +2,22 @@ import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import PricingColumn from './PricingColumn';
 import TimeBlock from './TimeBlock';
-import { Button } from '@jp-olvera/jp-viaducto-components';
+import { Button, Container, Spacer } from '@jp-olvera/jp-viaducto-components';
+import { getWeek } from '../../utils/getWeek';
+
 // TODO: OPACIDAD PARA LOS DÍAS QUE NO PERTENECEN A UN MES
 // TODO: INDICADOR PAR UN DÍA DEL MES ANTERIOR JULY/31
 // TODO: DAR CLICK EN UN MARCO PARA CREAR EL CONTEO
-// TODO: AGREGAR LA POSIBILIDAD DE AGREGAR EL MES DEL AÑO
 // TODO: AGREGAR LA POSIBILIDAD DE AGREGAR EL DÍA DEL AÑO
-// TODO: EVALUAR LA POSIBILIDAD DE FLECHAS PARA UNA SEMANA ANTES Y UNA ANTERIOR
 // TODO: I18N, RECIBIR LOS NOMBRES DE LOS MESES COMO PARÁMETROS PARA LA INTERNACIONALIZACIÓN
 // TODO: I18N, RECIBIR LOS NOMBRES DE LOS DÍAS DE LA SEMANA COMO PARÁMETROS PARA LA INTERNACIONALIZACIÓN
 // TODO: CÓMO MANEJAR LOS TEXTOS DE NOON, NIGHT
-// FIXME: EL BORDE DE LA COLUMNA DE HORAS NO DEBE SALIR COMPLETO
 // FIXME: CUANDO LLEGAS A JUNE 30 TODO SE VE GRIS
+// FIXME: CAMBIAR SEMANA MARCA MAL LAS FECHAS
 const StyledPricingTable = styled.div`
   display: grid;
   grid-template-columns: 90px repeat(7, minmax(11rem, 1fr));
-  grid-template-rows: 27px 1fr;
+  grid-template-rows: 50px 1fr;
   grid-auto-columns: 200px;
 
   @media (max-width: 768px) {
@@ -30,7 +30,9 @@ const StyledPricingTable = styled.div`
 
 interface PricingTableProps {
   months?: { [key: number]: string };
+  week_days?: { [key: number]: string };
 }
+
 const PricingTable = ({
   months = {
     0: 'JANUARY',
@@ -46,25 +48,34 @@ const PricingTable = ({
     10: 'NOVEMBER',
     11: 'DECEMBER',
   },
+  week_days = {
+    1: 'MON',
+    2: 'TUES',
+    3: 'WED',
+    4: 'THU',
+    5: 'FRI',
+    6: 'SAT',
+    0: 'SUN',
+  },
 }: PricingTableProps) => {
   const [month, setMonth] = useState<number>(1);
-  const [weekDates, setWeekDates] = useState<{ [key: number]: [number, number] }>({
-    0: [0, 0],
-    1: [0, 0],
-    2: [0, 0],
-    3: [0, 0],
-    4: [0, 0],
-    5: [0, 0],
-    6: [0, 0],
+  const [weekDates, setWeekDates] = useState<{ [key: string]: { day: number; month: number } }>({
+    sun: { day: 1, month: 0 },
+    mon: { day: 1, month: 0 },
+    tues: { day: 1, month: 0 },
+    wed: { day: 1, month: 0 },
+    thu: { day: 1, month: 0 },
+    fri: { day: 1, month: 0 },
+    sat: { day: 1, month: 0 },
   });
   const [changeWeeks, setChangeWeeks] = useState(0);
-  // const [todayDay, setTodayDay] = useState(0);
-  // const [todayMonth, setTodayMonth] = useState(1);
-  // useEffect(() => {
-  //   const d = new Date();
-  //   setTodayDay(d.getDay());
-  //   setTodayMonth(d.getMonth());
-  // }, []);
+  const [todayDay, setTodayDay] = useState(0);
+  const [todayMonth, setTodayMonth] = useState(1);
+  useEffect(() => {
+    const d = new Date();
+    setTodayDay(d.getDate());
+    setTodayMonth(d.getMonth());
+  }, []);
   const handleChangeWeek = (next = true) => {
     // goes forwards
     if (next) {
@@ -77,36 +88,17 @@ const PricingTable = ({
 
   useEffect(() => {
     const curr = new Date(); // today
-    curr.setDate(curr.getDate() + changeWeeks * 7); // adjust based on changeWeeks
-    console.log(curr);
-    setMonth(curr.getMonth());
-    const firstDay = curr.getDate() - curr.getDay();
+    var selectedWeek = new Date(curr.getTime() + changeWeeks * 7 * 24 * 60 * 60 * 1000); // adjust based on changeWeeks
+    setMonth(selectedWeek.getMonth());
+    const weekDays = getWeek(selectedWeek);
     setWeekDates({
-      0: [new Date(curr.setDate(firstDay)).getDate(), new Date(curr.setDate(firstDay)).getMonth()],
-      1: [
-        new Date(curr.setDate(firstDay + 1)).getDate(),
-        new Date(curr.setDate(firstDay + 1)).getMonth(),
-      ],
-      2: [
-        new Date(curr.setDate(firstDay + 2)).getDate(),
-        new Date(curr.setDate(firstDay + 2)).getMonth(),
-      ],
-      3: [
-        new Date(curr.setDate(firstDay + 3)).getDate(),
-        new Date(curr.setDate(firstDay + 3)).getMonth(),
-      ],
-      4: [
-        new Date(curr.setDate(firstDay + 4)).getDate(),
-        new Date(curr.setDate(firstDay + 4)).getMonth(),
-      ],
-      5: [
-        new Date(curr.setDate(firstDay + 5)).getDate(),
-        new Date(curr.setDate(firstDay + 5)).getMonth(),
-      ],
-      6: [
-        new Date(curr.setDate(firstDay + 6)).getDate(),
-        new Date(curr.setDate(firstDay + 6)).getMonth(),
-      ],
+      sun: { day: weekDays[0].getDate(), month: weekDays[0].getMonth() },
+      mon: { day: weekDays[1].getDate(), month: weekDays[1].getMonth() },
+      tues: { day: weekDays[2].getDate(), month: weekDays[2].getMonth() },
+      wed: { day: weekDays[3].getDate(), month: weekDays[3].getMonth() },
+      thu: { day: weekDays[4].getDate(), month: weekDays[4].getMonth() },
+      fri: { day: weekDays[5].getDate(), month: weekDays[5].getMonth() },
+      sat: { day: weekDays[6].getDate(), month: weekDays[6].getMonth() },
     });
   }, [changeWeeks]);
 
@@ -171,89 +163,121 @@ const PricingTable = ({
     return blocks;
   };
 
-  const WEEK_DAYS = {
-    1: 'MON',
-    2: 'TUES',
-    3: 'WED',
-    4: 'THU',
-    5: 'FRI',
-    6: 'SAT',
-    0: 'SUN',
-  };
   return (
     <>
-      <div style={{ display: 'flex', fontSize: '2rem' }}>
+      <Container
+        style={{ display: 'flex', fontSize: '1.5rem', alignItems: 'center' }}
+        expandHorizontal
+        top='md'
+      >
         <span>
           <b>{months[month]}</b>
         </span>
         <div style={{ marginLeft: 'auto' }}>
           <Button
             label='<'
-            shapeColor='success'
             variant='outline'
             onClick={() => {
               handleChangeWeek(false);
             }}
           />
+          <Spacer size='sm' direction='horizontal' />
           <Button
             label='>'
-            shapeColor='success'
             variant='outline'
             onClick={() => {
               handleChangeWeek();
             }}
           />
         </div>
-      </div>
+      </Container>
       <div style={{ width: '100%' }}>
         <StyledPricingTable>
           <div></div>
-          <div>
-            {WEEK_DAYS[0]} ({weekDates[0][0]} del {weekDates[0][1]})
+          <div
+            className={
+              todayDay === weekDates.sun.day && todayMonth === weekDates.sun.month ? 'is-today' : ''
+            }
+          >
+            {week_days[0]} {weekDates.sun.day}
           </div>
-          <div>
-            {WEEK_DAYS[1]} ({weekDates[1][0]} del {weekDates[1][1]})
+          <div
+            className={
+              todayDay === weekDates.mon.day && todayMonth === weekDates.mon.month ? 'is-today' : ''
+            }
+          >
+            {week_days[1]} {weekDates.mon.day}
           </div>
-          <div className='pc-hide'>
-            {WEEK_DAYS[2]} ({weekDates[2][0]} del {weekDates[2][1]})
+          <div
+            className={
+              todayDay === weekDates.tues.day && todayMonth === weekDates.tues.month
+                ? 'is-today pc-hide'
+                : 'pc-hide'
+            }
+          >
+            {week_days[2]} {weekDates.tues.day}
           </div>
-          <div className='pc-hide'>
-            {WEEK_DAYS[3]} ({weekDates[3][0]} del {weekDates[3][1]})
+          <div
+            className={
+              todayDay === weekDates.wed.day && todayMonth === weekDates.wed.month
+                ? 'is-today pc-hide'
+                : 'pc-hide'
+            }
+          >
+            {week_days[3]} {weekDates.wed.day}
           </div>
-          <div className='pc-hide'>
-            {WEEK_DAYS[4]} ({weekDates[4][0]} del {weekDates[4][1]})
+          <div
+            className={
+              todayDay === weekDates.thu.day && todayMonth === weekDates.thu.month
+                ? 'is-today pc-hide'
+                : 'pc-hide'
+            }
+          >
+            {week_days[4]} {weekDates.thu.day}
           </div>
-          <div className='pc-hide'>
-            {WEEK_DAYS[5]} ({weekDates[5][0]} del {weekDates[5][1]})
+          <div
+            className={
+              todayDay === weekDates.fri.day && todayMonth === weekDates.fri.month
+                ? 'is-today pc-hide'
+                : 'pc-hide'
+            }
+          >
+            {week_days[5]} {weekDates.fri.day}
           </div>
-          <div className='pc-hide'>
-            {WEEK_DAYS[6]} ({weekDates[6][0]} del {weekDates[6][1]})
+          <div
+            className={
+              todayDay === weekDates.sat.day && todayMonth === weekDates.sat.month
+                ? 'is-today pc-hide'
+                : 'pc-hide'
+            }
+          >
+            {week_days[6]} {weekDates.sat.day}
           </div>
           <div>{getTimeBlocks()}</div>
-          <PricingColumn isCurrentMonth={weekDates[0][1] === month} bidding={MONDAY} />
-          <PricingColumn isCurrentMonth={weekDates[1][1] === month} bidding={TUESDAY} />
+          <PricingColumn isCurrentMonth={weekDates.sun.month === month} bidding={MONDAY} />
+          <PricingColumn isCurrentMonth={weekDates.mon.month === month} bidding={TUESDAY} />
           <PricingColumn
-            isCurrentMonth={weekDates[2][1] === month}
+            isCurrentMonth={weekDates.tues.month === month}
             className='pc-hide'
             bidding={MONDAY}
           />
           <PricingColumn
-            isCurrentMonth={weekDates[3][1] === month}
+            isCurrentMonth={weekDates.wed.month === month}
             className='pc-hide'
             bidding={TUESDAY}
           />
           <PricingColumn
-            isCurrentMonth={weekDates[4][1] === month}
+            isCurrentMonth={weekDates.thu.month === month}
             className='pc-hide'
             bidding={MONDAY}
           />
           <PricingColumn
-            isCurrentMonth={weekDates[5][1] === month}
+            isCurrentMonth={weekDates.fri.month === month}
             className='pc-hide'
             bidding={TUESDAY}
           />
           <PricingColumn
-            isCurrentMonth={weekDates[6][1] === month}
+            isCurrentMonth={weekDates.sat.month === month}
             className='pc-hide'
             bidding={TUESDAY}
           />
