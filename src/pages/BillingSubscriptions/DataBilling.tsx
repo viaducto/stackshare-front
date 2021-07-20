7f7b0bbab18f7918a5d9864439bea10b5c483932
Jorge Rojas <jl.rojas@viaducto.io>
2021-07-20 15:20:21 -0500

    feat: add table to billing

---
+++ src/pages/BillingSubscriptions/DataBilling.tsx
@@ -0,0 +1,196 @@
+import { AvatarWithText, Container, Paragraph } from '@jp-olvera/jp-viaducto-components';
+import React from 'react';
+import Tag from '../../components/Tag/Tag';
+import master from './mastercard.svg';
+
+export const Center = ({ children, ...rest }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div style={{ display: 'flex', width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+    {children}
+  </div>
+);
+
+export const Payment = () => (
+  <Container vertical='sm' horizontal='xs'>
+    <AvatarWithText avatar={{ src: master, width: '25', height: '25', alt: 'card' }}>
+      <div style={{ flexDirection: 'column' }}>
+        <Paragraph color='#3E3F42' lineHeight='100%'>
+          XXXX-XXXX-XXXX-1345
+        </Paragraph>
+        <Paragraph color='#838383' lineHeight='100%' size='sm'>
+          Master Card - Debit
+        </Paragraph>
+      </div>
+    </AvatarWithText>
+  </Container>
+);
+
+export const columns = [
+  {
+    accessor: 'invoice_id',
+    Header: 'Invoice ID',
+    minWidth: 150,
+    width: 150,
+    maxWidth: 180,
+    Cell: (props: any) => <Center>{props.data[props.row.index].invoice_id}</Center>,
+  },
+  {
+    accessor: 'date',
+    Header: 'Due Date',
+    minWidth: 200,
+    width: 200,
+    maxWidth: 250,
+    Cell: (props: any) => <Center>{props.data[props.row.index].date}</Center>,
+  },
+  {
+    accessor: 'pay',
+    Header: 'Payment Due',
+    minWidth: 250,
+    width: 250,
+    maxWidth: 300,
+    Cell: (props: any) => <Center>{props.data[props.row.index].pay}</Center>,
+  },
+  {
+    accessor: 'amount',
+    Header: 'Amount',
+    minWidth: 180,
+    width: 180,
+    maxWidth: 230,
+    Cell: (props: any) => <Center>{props.data[props.row.index].amount}</Center>,
+  },
+  {
+    accessor: 'status',
+    Header: 'Status',
+    minWidth: 150,
+    width: 150,
+    maxWidth: 180,
+    Cell: (props: any) => <Center>{props.data[props.row.index].status}</Center>,
+  },
+  {
+    accessor: 'method',
+    Header: 'Method',
+    minWidth: 300,
+    width: 300,
+    maxWidth: 350,
+    Cell: (props: any) => <Center>{props.data[props.row.index].method}</Center>,
+  },
+];
+
+export const data = [
+  {
+    invoice_id: '389.29',
+    date: 'March 4, 2018',
+    pay: 'March 4, 2018',
+    amount: '$282',
+    status: (
+      <Center>
+        <Tag label='Paid' color='#52C41A' backgroundColor='#F6FFED' borderColor='#B7EB8F' />
+      </Center>
+    ),
+    method: <Payment />,
+    id: '1',
+  },
+  {
+    invoice_id: '267.41',
+    date: 'April 6, 2014',
+    pay: 'March 4, 2018',
+    amount: '$884',
+    status: (
+      <Center>
+        <Tag label='Paid' color='#52C41A' backgroundColor='#F6FFED' borderColor='#B7EB8F' />
+      </Center>
+    ),
+    method: <Payment />,
+    id: '2',
+  },
+  {
+    invoice_id: '751.84',
+    date: 'May 17, 2015',
+    pay: 'March 4, 2018',
+    amount: '$513',
+    status: (
+      <Center>
+        <Tag label='Unpaid' color='#F5222D' backgroundColor='#FFF1F0' borderColor='#FFA39E' />
+      </Center>
+    ),
+    method: <Payment />,
+    id: '3',
+  },
+  {
+    invoice_id: '280.40',
+    date: 'July 21, 2015',
+    pay: 'March 4, 2018',
+    amount: '$729',
+    status: (
+      <Center>
+        <Tag label='Paid' color='#52C41A' backgroundColor='#F6FFED' borderColor='#B7EB8F' />
+      </Center>
+    ),
+    method: <Payment />,
+    id: '4',
+  },
+  {
+    invoice_id: '512.14',
+    date: 'March 1, 2020',
+    pay: 'March 4, 2018',
+    amount: '$493',
+    status: (
+      <Center>
+        <Tag label='Paid' color='#52C41A' backgroundColor='#F6FFED' borderColor='#B7EB8F' />
+      </Center>
+    ),
+    method: <Payment />,
+    id: '5',
+  },
+  {
+    invoice_id: '584.61',
+    date: 'May 3, 2014',
+    pay: 'March 4, 2018',
+    amount: '$348',
+    status: (
+      <Center>
+        <Tag label='Paid' color='#52C41A' backgroundColor='#F6FFED' borderColor='#B7EB8F' />
+      </Center>
+    ),
+    method: <Payment />,
+    id: '6',
+  },
+  {
+    invoice_id: '581.68',
+    date: 'February 20, 2015',
+    pay: 'March 4, 2018',
+    amount: '$849',
+    status: (
+      <Center>
+        <Tag label='Pending' color='#FA8C16' backgroundColor='#FFF7E6' borderColor='#FFD591' />
+      </Center>
+    ),
+    method: <Payment />,
+    id: '7',
+  },
+  {
+    invoice_id: '162.17',
+    date: 'December 14, 2016',
+    pay: 'March 4, 2018',
+    amount: '$949',
+    status: (
+      <Center>
+        <Tag label='Paid' color='#52C41A' backgroundColor='#F6FFED' borderColor='#B7EB8F' />
+      </Center>
+    ),
+    method: <Payment />,
+    id: '8',
+  },
+  {
+    invoice_id: '736.32',
+    date: 'May 1, 2018',
+    pay: 'March 4, 2018',
+    amount: '$620',
+    status: (
+      <Center>
+        <Tag label='Paid' color='#52C41A' backgroundColor='#F6FFED' borderColor='#B7EB8F' />
+      </Center>
+    ),
+    method: <Payment />,
+    id: '9',
+  },
+];
--- src/pages/BillingSubscriptions/SubscriptionsTable.tsx
+++ src/pages/BillingSubscriptions/SubscriptionsTable.tsx
@@ -1,7 +1,41 @@
+import { Column, Container, Spacer, WrapperTable } from '@jp-olvera/jp-viaducto-components';
 import React from 'react';
+import Table from '../../components/Table/Table';
+import Kebab from '../../components/Kebab/Kebab';
+import { columns, data } from './DataBilling';
 
 const SubscriptionsTable = () => {
-  return <div>Table</div>;
+  return (
+    <Column>
+      <WrapperTable align='center' zebra={false} border='horizontal'>
+        <Table
+          pagination={false}
+          filter={false}
+          cols={[
+            ...columns,
+            {
+              accessor: 'action',
+              Header: 'Action',
+              minWidth: 100,
+              width: 100,
+              maxWidth: 230,
+              Cell: () => (
+                <div style={{ width: '100%' }}>
+                  <Kebab>
+                    <Container vertical='md' horizontal='md'>
+                      Table action
+                    </Container>
+                  </Kebab>
+                </div>
+              ),
+            },
+          ]}
+          dataTable={data}
+        />
+      </WrapperTable>
+      <Spacer size='xl' />
+    </Column>
+  );
 };
 
 export default SubscriptionsTable;
