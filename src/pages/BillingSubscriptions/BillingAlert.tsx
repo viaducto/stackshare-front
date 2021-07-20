44a101597bf6056241140866314993ad4fc631c1
jp-olvera <jp.olvera@viaducto.io>
2021-07-20 15:27:06 -0500

    feat: add billing alerts and manager

--- src/pages/BillingSubscriptions/BillingAlert.tsx
+++ src/pages/BillingSubscriptions/BillingAlert.tsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import { Column, Switch, Paragraph, Container } from '@jp-olvera/jp-viaducto-components';
 
 const BillingAlert = () => {
-  return <div>alert</div>;
+  return (
+    <Column xs={12} sm={8} md={6}>
+      <Container vertical='md'>
+        <Paragraph size='lg' color='dark'>
+          Email Notifications
+        </Paragraph>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+          }}
+        >
+          <Paragraph color='gray' size='lg'>
+            Failed transaction
+          </Paragraph>
+          <Switch inputSize='sm' defaultChecked color='#1890FF' />
+        </div>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+          }}
+        >
+          <Paragraph color='gray' size='lg'>
+            Failed transaction
+          </Paragraph>
+          <Switch inputSize='sm' defaultChecked color='#1890FF' />
+        </div>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+          }}
+        >
+          <Paragraph color='gray' size='lg'>
+            Failed transaction
+          </Paragraph>
+          <Switch inputSize='sm' defaultChecked color='#1890FF' />
+        </div>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+          }}
+        >
+          <Paragraph color='gray' size='lg'>
+            Failed transaction
+          </Paragraph>
+          <Switch inputSize='sm' defaultChecked color='#1890FF' />
+        </div>
+      </Container>
+    </Column>
+  );
 };
 
 export default BillingAlert;
--- src/pages/BillingSubscriptions/BillingManager.tsx
+++ src/pages/BillingSubscriptions/BillingManager.tsx
@@ -1,7 +1,110 @@
-import { Column } from '@jp-olvera/jp-viaducto-components';
+import {
+  Column,
+  AvatarWithText,
+  Paragraph,
+  Container,
+  Switch,
+} from '@jp-olvera/jp-viaducto-components';
 
 const BillingManager = () => {
-  return <Column>manager</Column>;
+  return (
+    <>
+      <Column xs={12} md={4}>
+        <Container vertical='md'>
+          <div className='flex'>
+            <AvatarWithText
+              avatar={{
+                src: 'https://i.mdel.net/i/db/2020/4/1332723/1332723-500w.jpg',
+                alt: '',
+              }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+                <Paragraph color='dark' size='lg'>
+                  John Doe
+                </Paragraph>
+                <Paragraph color='darkGray'>john@ballena.com</Paragraph>
+              </div>
+            </AvatarWithText>
+          </div>
+        </Container>
+      </Column>
+      <Column xs={12} md={4}>
+        <Paragraph size='lg' color='dark'>
+          Permissions
+        </Paragraph>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+          }}
+        >
+          <Paragraph color='gray' size='lg'>
+            Upgrade plan
+          </Paragraph>
+          <Switch inputSize='sm' defaultChecked color='#1890FF' />
+        </div>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+          }}
+        >
+          <Paragraph color='gray' size='lg'>
+            Downgrade plan
+          </Paragraph>
+          <Switch inputSize='sm' defaultChecked color='#1890FF' />
+        </div>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+          }}
+        >
+          <Paragraph color='gray' size='lg'>
+            Change Billing Information
+          </Paragraph>
+          <Switch inputSize='sm' defaultChecked color='#1890FF' />
+        </div>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+          }}
+        >
+          <Paragraph color='gray' size='lg'>
+            Download Invoices
+          </Paragraph>
+          <Switch inputSize='sm' defaultChecked color='#1890FF' />
+        </div>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+          }}
+        >
+          <Paragraph color='gray' size='lg'>
+            Permissions
+          </Paragraph>
+          <Switch inputSize='sm' defaultChecked color='#1890FF' />
+        </div>
+      </Column>
+    </>
+  );
 };
 
 export default BillingManager;
